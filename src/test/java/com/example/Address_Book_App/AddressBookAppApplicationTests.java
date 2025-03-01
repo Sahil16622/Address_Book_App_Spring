d56687afbbdbfee3adea0242cb5be2f53e3e6a3f
Sahil Singh Chauhan <sahilchauhan5467@gmail.com>
2025-03-01 15:27:54 +0530

    Initialized git repository for Address book

---
+++ src/test/java/com/example/Address_Book_App/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Address_Book_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
